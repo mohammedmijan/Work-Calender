3f775fc11eac719076349d714106f438f9733927
mijan <mejan601@gmail.com>
2021-08-20 15:13:49 +0600

    Login system check

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = ""
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
